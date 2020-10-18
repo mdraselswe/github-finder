e0bba575f8a07c2af652cab3f0d7bbd8a03afe50
Rasel <rrasel141@gmail.com>
2020-10-18 19:55:11 +0600

    add - found page & cleanup app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import User from './components/users/User';
-import Users from './components/users/Users';
+import NotFound from './components/pages/NotFound';
 import AlertState from './context/alert/AlertState';
 import GithubState from './context/github/GithubState';
 
@@ -20,18 +20,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
